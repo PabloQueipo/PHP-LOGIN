bcead4e72cb6ef19f7902adf6db7b6d4d7389fa0
Pablo Queipo <55402074+PabloQueipo@users.noreply.github.com>
2021-03-01 19:12:17 +0100

    Create principal.php

---
+++ principal.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Pagina Queipo</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    
+    <div class="cuerpo">
+        <div class="contenido">
+            <h1>Bienvenido</h1>
+        </div>
+        <a class="boton" href="http://localhost/PHP-LOGIN/suma.php">Suma</a>
+		<a class="boton" href="http://localhost/PHP-LOGIN/posicion.html">Guardar Localización GPS</a>
+		<a class="boton" href="http://localhost/PHP-LOGIN/listar.php">Listar Procesos</a>
+    <div class="pie">
+	<p>© Copyright Queipo</p>
+    </div>
+</body>
+</html>
